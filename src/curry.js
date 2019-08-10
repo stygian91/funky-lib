0ebd64e6ed18e10630066d61c035ac9d3e0c7f22
Stygian <kioravuser@gmail.com>
2019-08-10 18:58:34 +0300

    Make the curry function return the original function's result if no arguments are curried. Fix curry function tests not properly testing the argument's order

--- src/curry.js
+++ src/curry.js
@@ -1,19 +1,25 @@
 const __ = require('./placeholderArgument');
 
-const curry = func => (...args) => (...curriedArgs) => {
-  const actualArgs = [];
-  let curryIndex = 0;
-
-  for (let index = 0; index < func.length; index++) {
-    if (args[index] === __ || index >= args.length) {
-      actualArgs.push(curriedArgs[curryIndex]);
-      curryIndex++;
-    } else {
-      actualArgs.push(args[index]);
-    }
+const curry = func => (...args) => {
+  if (!args.includes(__) && func.length <= args.length) {
+    return func(...args);
   }
 
-  return func(...actualArgs);
+  return (...curriedArgs) => {
+    const actualArgs = [];
+    let curryIndex = 0;
+
+    for (let index = 0; index < func.length; index++) {
+      if (args[index] === __ || index >= args.length) {
+        actualArgs.push(curriedArgs[curryIndex++]);
+      } else {
+        actualArgs.push(args[index]);
+      }
+    }
+
+    return func(...actualArgs);
+  };
 };
+
 
 module.exports = curry;
--- test/curry.test.js
+++ test/curry.test.js
@@ -1,28 +1,35 @@
 const curry = require('../src/curry');
 const __ = require('../src/placeholderArgument');
 
-const operation = (a, b, c) => a - b + c;
-const biggerOperation = (a, b, c, d) => a - b + c - d;
+const operation = (a, b, c) => a * b + c;
+const biggerOperation = (a, b, c, d) => a * b + c * d;
 
 test('curries all arguments after the last supplied in the right order', () => {
   const curriedOperation = curry(operation);
   const binaryOperation = curriedOperation(1);
   const unaryOperation = curriedOperation(1, 2);
 
-  expect(binaryOperation(2, 3)).toBe(2);
-  expect(unaryOperation(4)).toBe(3);
+  expect(binaryOperation(2, 3)).toBe(5);
+  expect(unaryOperation(4)).toBe(6);
 });
 
 test('curries placeholder arguments in the right order', () => {
   const curriedOperation = curry(operation);
   const missingSecondArgOp = curriedOperation(1, __, 3);
 
-  expect(missingSecondArgOp(2)).toBe(2);
+  expect(missingSecondArgOp(2)).toBe(5);
 });
 
 test('curries placeholder and remaining arguments in the right order', () => {
   const curriedOperation = curry(biggerOperation);
   const missingArgsOp = curriedOperation(1, __, 3);
 
-  expect(missingArgsOp(2, 4)).toBe(-2);
+  expect(missingArgsOp(2, 4)).toBe(14);
+});
+
+test('calling with all arguments just returns the result', () => {
+  const curriedOperation = curry(biggerOperation);
+
+  const operationResult = curriedOperation(1, 2, 3, 4);
+  expect(operationResult).toBe(14);
 });
