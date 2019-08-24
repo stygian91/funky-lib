7dfadd63a46471b2e03bbbb4aa488e45b42a2a79
Stygian <kioravuser@gmail.com>
2019-08-24 18:33:04 +0300

    Add memoizeWith and tap functions

---
+++ src/function/memoizeWith.js
@@ -0,0 +1,17 @@
+import curry from './curry';
+
+const memoizeWith = (keyGenerator, func) => {
+  const cache = {};
+
+  return (...args) => {
+    const key = keyGenerator(args);
+    if (!cache.hasOwnProperty(key)) {
+      const result = func(...args);
+      cache[key] = result;
+    }
+
+    return cache[key];
+  };
+}
+
+export default curry(memoizeWith);
---
+++ src/function/tap.js
@@ -0,0 +1,8 @@
+import curry from './curry';
+
+const tap = (func, arg) => {
+  func(arg);
+  return arg;
+}
+
+export default curry(tap);
