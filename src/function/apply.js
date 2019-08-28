a19a31dbcdb8d11f9bd3251ac81770263d47d80d
Stygian <kioravuser@gmail.com>
2019-08-29 00:06:56 +0300

    Add apply function

---
+++ src/function/apply.js
@@ -0,0 +1,7 @@
+import curry from './curry';
+
+const apply = (func, args) => {
+  return func.apply(void 0, args);
+};
+
+export default curry(apply);
---
+++ src/function/apply.test.js
@@ -0,0 +1,20 @@
+import apply from './apply';
+
+test('it applies the list of arguments to the function', () => {
+  let func = (a, b) => a + b;
+  const result = apply(func, [1, 2]);
+  expect(result).toEqual(3);
+});
+
+test('it passes undefined as default context', () => {
+  let func = function() { return this; }
+  const result = apply(func, []);
+  expect(typeof result).toEqual('undefined');
+});
+
+test("it doesn't change a bound function's context", () => {
+  let obj = {};
+  let func = function() { this['someproperty'] = 'test'; }.bind(obj);
+  apply(func, []);
+  expect(obj['someproperty']).toEqual('test');
+});
