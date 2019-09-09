75771c28eaaae461146ee74a79936b2f0f839e9f
Stygian <kioravuser@gmail.com>
2019-09-09 22:42:14 +0300

    Add 'has' function

---
+++ src/object/has.js
@@ -0,0 +1,5 @@
+import curry from '../function/curry';
+
+const has = (prop, object) => object.hasOwnProperty(prop);
+
+export default curry(has);
--- src/object/index.js
+++ src/object/index.js
@@ -1,4 +1,5 @@
 export { default as assoc }         from './assoc';
+export { default as has }           from './has';
 export { default as path }          from './path';
 export { default as pathOr }        from './pathOr';
 export { default as pathSatisfies } from './pathSatisfies';
