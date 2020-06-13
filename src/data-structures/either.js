49bfeea2016ec102d12e062cb5cb25b451a146e2
Stygian <kioravuser@gmail.com>
2020-06-13 23:53:46 +0300

    Add either data structure

---
+++ src/data-structures/either.js
@@ -0,0 +1,97 @@
+export class Either {
+  constructor(x) {
+    this.value = x;
+  }
+
+  static of(x) {
+    return new Right(x);
+  }
+}
+
+export class Left extends Either {
+  get isLeft() {
+    return true;
+  }
+
+  get isRight() {
+    return false;
+  }
+
+  static of(x) {
+    throw new Error(
+      "`of` called on class Left (value) instead of Either (type)"
+    );
+  }
+
+  equals(other) {
+    return other instanceof Left;
+  }
+
+  map() {
+    return this;
+  }
+
+  ap() {
+    return this;
+  }
+
+  chain() {
+    return this;
+  }
+
+  join() {
+    return this;
+  }
+
+  sequence(of) {
+    return of(this);
+  }
+
+  traverse(of, fn) {
+    return of(this);
+  }
+}
+
+export class Right extends Either {
+  get isLeft() {
+    return false;
+  }
+
+  get isRight() {
+    return true;
+  }
+
+  static of(x) {
+    throw new Error(
+      "`of` called on class Right (value) instead of Either (type)"
+    );
+  }
+
+  equals(other) {
+    return other instanceof Right && this.value === other.value;
+  }
+
+  map(fn) {
+    return Either.of(fn(this.value));
+  }
+
+  ap(f) {
+    return f.map(this.value);
+  }
+
+  chain(fn) {
+    return this.map(fn).join();
+  }
+
+  join() {
+    return this.value;
+  }
+
+  sequence(of) {
+    return this.traverse(of, (x) => x);
+  }
+
+  traverse(of, fn) {
+    return fn(this.value).map(Either.of);
+  }
+}
---
+++ src/data-structures/either.test.js
@@ -0,0 +1,48 @@
+import { expectEquals, expectType } from "../internals/_test-helpers";
+import { Left, Right, Either } from "./either";
+import Identity from "./identity";
+
+const expectRightEquals = expectEquals(Right);
+const expectLeftEquals = expectEquals(Left);
+
+test("Either", () => {
+  const either = Either.of(1);
+  expectRightEquals(new Right(1), either);
+  expect(either.isRight).toEqual(true);
+  expect(either.isLeft).toEqual(false);
+  expect(() => Right.of(2)).toThrow();
+  expect(either.chain((x) => x + 5)).toEqual(6);
+  const rightOfFunc = Either.of((x) => x + 3);
+  expect(rightOfFunc.ap(Identity.of(5)).join()).toEqual(8);
+  const rightOfId = Either.of(Identity.of(10));
+  const idOfRight = rightOfId.sequence(Identity.of);
+  expectType(Identity, idOfRight);
+  expectRightEquals(idOfRight.join(), Either.of(10));
+
+  // Left:
+  expect(() => Left.of(2)).toThrow();
+  expectLeftEquals(new Left(), new Left());
+  expect(new Left().isLeft).toEqual(true);
+  expect(new Left().isRight).toEqual(false);
+  expect(Either.of(1).chain((x) => x * 3)).toEqual(3);
+  expectLeftEquals(
+    new Left(),
+    new Left().chain((x) => x + 4)
+  );
+  expectLeftEquals(
+    new Left(),
+    new Left().map((x) => x + 4)
+  );
+  expectLeftEquals(
+    new Left(),
+    new Left().join((x) => x + 4)
+  );
+  expectLeftEquals(new Left(), new Left().ap(Identity.of(() => x * 2)));
+
+  const idOfLeft = Identity.of(new Left());
+  const idOfLeft2 = new Left().sequence(Identity.of);
+  expectLeftEquals(idOfLeft.join(), idOfLeft2.join());
+
+  const idOfLeft3 = new Left().traverse(Identity.of);
+  expectLeftEquals(idOfLeft.join(), idOfLeft3.join());
+});
