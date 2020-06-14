604da3bc5ae59e6a93a37c56b750c5f738f30a2f
Stygian <kioravuser@gmail.com>
2020-06-14 14:03:37 +0300

    Add missing exports

--- src/data-structures/index.js
+++ src/data-structures/index.js
@@ -1,2 +1,5 @@
 export { default as Maybe } from "./maybe";
 export { default as List } from "./list";
+export { default as IO } from "./io";
+export { default as Identity } from "./identity";
+export * from "./either";
