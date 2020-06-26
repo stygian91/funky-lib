4645e2851aaacdcfa3ca2061f4c953ba6d21ae5b
Stygian <kioravuser@gmail.com>
2020-06-26 15:14:32 +0300

    Update dissocPath to always be immutable.

--- src/object/dissocPath.js
+++ src/object/dissocPath.js
@@ -8,7 +8,7 @@ import slice from "../list/slice";
 const dissocPath = (path, object) => {
   const pathArray = _getPathArray(path);
   if (pathArray.length === 0) {
-    return object;
+    return { ...object };
   }
 
   const prop = pathArray[0];
@@ -19,7 +19,7 @@ const dissocPath = (path, object) => {
   }
 
   if (isNullish(object[prop])) {
-    return object;
+    return { ...object };
   }
 
   return assoc(prop, dissocPath(remainingPath, object[prop]), object);
