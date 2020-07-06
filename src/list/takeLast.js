2391be5ccbb94bd394f4347a1bd83bd89dd0f3cf
Stygian <kioravuser@gmail.com>
2020-07-06 22:35:50 +0300

    Fix off-by-one error in `takeLast`. The the error was not impacting the functional behaviour of the function, because of how the native `slice` function works.

--- src/list/takeLast.js
+++ src/list/takeLast.js
@@ -2,6 +2,6 @@ import curry from "../function/curry";
 import slice from "./slice";
 
 const takeLast = (numElements, list) =>
-  slice(-1 * numElements, list.length + 1, list);
+  slice(-1 * numElements, list.length, list);
 
 export default curry(takeLast);
