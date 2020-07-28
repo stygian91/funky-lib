4ec08d4ae520428e14c072d1fd1ede6f90d53c67
Stygian <kioravuser@gmail.com>
2020-07-29 00:01:15 +0300

    Simplify the converge function's test.

--- src/function/converge.test.js
+++ src/function/converge.test.js
@@ -1,5 +1,4 @@
 import converge from "./converge";
-import compose from "./compose";
 import reduce from "../list/reduce";
 import add from "../math/add";
 import div from "../math/divide";
@@ -7,7 +6,7 @@ import prop from "../object/prop";
 
 test("it executes the branch functions and passes their results to the converging function", () => {
   const sum = reduce(add, 0);
-  const average = converge(div, [sum, compose((x) => x, prop("length"))]);
+  const average = converge(div, [sum, prop("length")]);
 
   const result = average([1, 2, 3, 4, 5]);
   expect(result).toBe(3);
