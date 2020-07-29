c0015d15b57cdbf208605653363acb3f6e9575ef
Stygian <kioravuser@gmail.com>
2020-07-29 22:03:11 +0300

    Prettier formatting.

--- src/list/size.js
+++ src/list/size.js
@@ -1,15 +1,20 @@
 import { curry } from "../function";
 
 const size = (collection) => {
-  if(Array.isArray(collection) || typeof collection === "string") {
+  if (Array.isArray(collection) || typeof collection === "string") {
     return collection.length;
   }
 
-  if(typeof collection === "object") {
-    return Object.getOwnPropertyNames(collection).length + Object.getOwnPropertySymbols(collection).length;
+  if (typeof collection === "object") {
+    return (
+      Object.getOwnPropertyNames(collection).length +
+      Object.getOwnPropertySymbols(collection).length
+    );
   }
 
-  throw new Error(`Cannot determine the size of a variable with a type of ${typeof collection}.`);
-}
+  throw new Error(
+    `Cannot determine the size of a variable with a type of ${typeof collection}.`
+  );
+};
 
 export default curry(size);
