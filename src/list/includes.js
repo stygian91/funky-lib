f56dd3cff02c78ea8b22c7652963b59811fad8a8
Stygian <kioravuser@gmail.com>
2020-08-09 03:25:43 +0300

    Update docs of the includes function.

--- src/list/includes.js
+++ src/list/includes.js
@@ -2,7 +2,7 @@ import { curry } from "../";
 
 /**
  * Checks if the list includes `element, starting from `fromIndex`.
- * Defers to [Array.prototype.includes]{}
+ * Defers to [Array.prototype.includes]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes}
  *
  * @param {any} element
  * @param {number} fromIndex
