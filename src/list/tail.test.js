f78805e95042c885965e1970046095079eb8f6e2
Stygian <kioravuser@gmail.com>
2020-08-11 22:03:13 +0300

    Improve tests for the tail function.

--- src/list/tail.test.js
+++ src/list/tail.test.js
@@ -3,4 +3,8 @@ import { tail } from "./";
 test("tail", () => {
   expect(tail("qwerty")).toEqual("werty");
   expect(tail([2, 3, 4, 5])).toEqual([3, 4, 5]);
+  expect(tail([])).toEqual([]);
+  expect(tail("")).toEqual("");
+  expect(tail([1])).toEqual([]);
+  expect(tail("a")).toEqual("");
 });
