ae05506bec7e0db0acec7b575106be91d724d44f
Stygian <kioravuser@gmail.com>
2020-08-19 22:23:39 +0300

    Update docs for the union function.

--- src/list/union.js
+++ src/list/union.js
@@ -1,6 +1,18 @@
 import unionWith from "./unionWith";
 import { equals } from "..";
 
+/**
+ * Returns the union of the two lists.
+ * Uses the `equals` function to compare if two values are equal.
+ *
+ * @function
+ * @name union
+ * @see unionWith
+ * @see equals
+ * @param {any[]} list1
+ * @param {any[]} list2
+ * @returns {any[]}
+ */
 const union = unionWith(equals);
 
 export default union;
