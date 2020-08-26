e2590e0270d9dbf6588720369097d4d80711b2b2
Stygian <kioravuser@gmail.com>
2020-08-26 23:29:18 +0300

    Improve arity function docs.

--- src/function/arity.js
+++ src/function/arity.js
@@ -2,11 +2,13 @@
 /**
  * Create a new function with the specified arity between 0 and 10.
  *
+ * @function
+ * @name arity
  * @param {number} n
  * @param {function} fn
  * @returns {function}
  */
-export default function _arity(n, fn) {
+export default function arity(n, fn) {
   switch (n) {
     case 0:
       return function () {
