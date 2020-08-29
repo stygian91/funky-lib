24e44a6abb51a8682359e833f176a241394c0321
Stygian <kioravuser@gmail.com>
2020-08-29 11:18:38 +0300

    Improve tests for the isEmpty function.

--- src/logic/isEmpty.test.js
+++ src/logic/isEmpty.test.js
@@ -6,7 +6,13 @@ test("isEmpty", () => {
   expect(isEmpty(true)).toEqual(false);
   expect(isEmpty(0)).toEqual(false);
   expect(isEmpty(123)).toEqual(false);
+
   expect(isEmpty([])).toEqual(true);
   expect(isEmpty({})).toEqual(true);
   expect(isEmpty("")).toEqual(true);
+
+  expect(isEmpty([1, 2])).toEqual(false);
+  expect(isEmpty({ a: 1 })).toEqual(false);
+  expect(isEmpty({ [Symbol("mySymbol")]: "test" })).toEqual(false);
+  expect(isEmpty("lorem")).toEqual(false);
 });
