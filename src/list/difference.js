6eb50518d8881366ea740bfba02dc0ad68a47ef1
Stygian <kioravuser@gmail.com>
2020-08-30 11:14:38 +0300

    Fix difference function docs.

--- src/list/difference.js
+++ src/list/difference.js
@@ -6,6 +6,8 @@ import { curry } from "../function";
  * Returns a unique list of elements from list1 that are not present in list2.
  * Comparison is done using the `equals` function.
  *
+ * @function
+ * @name difference
  * @see differenceWith
  * @see equals
  * @param {any[]} list1
