468de768fbcd0c8f65de4399e1f4db557d0679fe
Stygian <kioravuser@gmail.com>
2020-10-06 22:28:38 +0300

    Update `range` function by reusing the `unfold` function.

--- src/list/range.js
+++ src/list/range.js
@@ -1,21 +1,17 @@
 import { curry } from "../function";
+import { unfold } from "../list";
+import { inRange } from "../math";
 
 /**
  * Returns a list of numbers from the first argument(inclusive)
  * to the second argument(exclusive).
  *
+ * @function
  * @param {number} from
  * @param {number} to
  * @returns {number[]}
  */
-const range = (from, to) => {
-  const result = [];
-
-  for (let i = from; i < to; i++) {
-    result.push(i);
-  }
-
-  return result;
-};
+const range = (from, to) =>
+  unfold((seed) => (inRange(from, to, seed) ? [seed, seed + 1] : false), from);
 
 export default curry(range);
